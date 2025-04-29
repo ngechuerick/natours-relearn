ebac4458ce5d17181bdec36268f26db487012e3b
ngechuerick <erick.ngechu3@gmail.com>
2025-04-29 22:56:12 +0300

    fixed bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -67,7 +67,7 @@ const createBookingCheckout = async function (session) {
 
   const user = (await User.findOne({ email: session.customer_email })).id;
 
-  const price = session.line_items[0].unit_amount / 100;
+  const price = session.line_items[0].price_data.unit_amount / 100;
 
   await Booking.create({ tour, user, price });
 };
